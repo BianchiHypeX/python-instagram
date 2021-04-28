2e8fd89edd1ba7706538aad49edbcce8315611e7
Bianchi Hyperx <68603244+BianchiHypeX@users.noreply.github.com>
2021-04-28 14:04:48 -0300

    Create equacao.py

---
+++ equacao.py
@@ -0,0 +1,21 @@
+# equacao em segundo grau deu mt trabalho kkkkk
+# instagram: @cake.js
+
+while True:
+    print('======================= MADE BY BIANCHI =======================')
+    print('Bem-vindo a calculadora de equação de 2º grau!')
+    a= float(input('Digita o coeficiente de a= '))
+    b= float(input('Digita o coeficiente de b= '))
+    c= float(input('Digita o coeficiente de c= '))
+    delta = b**2-4*a*c 
+    print('delta = ', delta)
+    raizdelta=delta**0.5
+    print('raiz de delta = ', raizdelta)
+    x1=(-b+raizdelta)/(2*a)
+    print('x1 = ', x1)
+    x2=(-b-raizdelta)/(2*a)
+    print('x2 = ', x2)
+    continue
+
+# caso for reproduzir isso em um video ou tutorial, dê os créditos
+# feito com ❤ por bianchi
